d12db0a125762765f6be7d4831f07ee6c5f2f8ed
Federico Esparza <federicoesparza@outlook.com>
2018-10-25 05:59:36 -0500

    Encase case statement into retrieveEpisode function. Move to context

--- lib/av_retrospective_podcasts.ex
+++ lib/av_retrospective_podcasts.ex
@@ -5,14 +5,5 @@ defmodule AvRetrospectivePodcasts do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
-  case HTTPoison.get("https://www.agileventures.org/events/av-retrospective") do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-      Floki.find(body, ".yt_link")
-      |> Floki.attribute("id")
-      |> Enum.at(0)
-    {:ok, %HTTPoison.Response{status_code: 404}} ->
-      IO.puts "Not found :("
-    {:error, %HTTPoison.Error{reason: reason}} ->
-      IO.inspect reason
-  end
+
 end
--- lib/av_retrospective_podcasts/podcast/episodes.ex
+++ lib/av_retrospective_podcasts/podcast/episodes.ex
@@ -13,4 +13,18 @@ defmodule AvRetrospectivePodcasts.Podcast.Episodes do
     |> cast(attrs, [:youtube_id])
     |> validate_required([:youtube_id])
   end
+
+
+  def retrieveEpisode do
+    case HTTPoison.get("https://www.agileventures.org/events/av-retrospective") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        Floki.find(body, ".yt_link")
+        |> Floki.attribute("id")
+        |> Enum.at(0)
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
 end
